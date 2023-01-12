c5d3da484d714b248a71d18beac789f2b2cc8180
AYUSHI ARYA <83390652+ayushiarya-00@users.noreply.github.com>
2023-01-12 12:09:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
-
 function palindrome(str){
-
+const alphanumericOnly = str.toLowerCase().match(/[a-z0-9]/g);
+        
+return alphanumericOnly.join('') ===
+        alphanumericOnly.reverse().join('');
 }
 module.exports = palindrome
